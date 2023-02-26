544110b598b97cf55cfa85e43d23ca888fee867d
Tito Rivera <titorivera0707@gmail.com>
2023-02-26 12:34:51 -0800

    Initial Commit

---
+++ MyStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStory
+//
+//  Created by Tito Rivera on 2/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
